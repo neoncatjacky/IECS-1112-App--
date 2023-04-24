4c43e5be2c4231cf1f09ce1c96954c691186f843
neoncatjacky <jackylanpoison97141@gmail.com>
2023-04-24 15:14:32 +0800

    temp commit on main

---
+++ app/src/main/java/fcu/app/breakfast/ViewCartActivity.java
@@ -0,0 +1,14 @@
+package fcu.app.breakfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewCartActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_view_cart);
+  }
+}
--- app/src/main/java/fcu/app/breakfast/ViewMenuActivity.java
+++ app/src/main/java/fcu/app/breakfast/ViewMenuActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ViewMenu extends AppCompatActivity {
+public class ViewMenuActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
