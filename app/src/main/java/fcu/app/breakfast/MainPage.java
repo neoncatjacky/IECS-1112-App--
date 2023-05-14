4ff0b73ded85cb833cbb0fcc0e3988bb16b443d2
mch <margot.cheung@gmail.com>
2023-05-14 19:24:11 +0800

    MainPage edited

--- app/src/main/java/fcu/app/breakfast/MainPage.java
+++ app/src/main/java/fcu/app/breakfast/MainPage.java
@@ -2,24 +2,28 @@ package fcu.app.breakfast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
-    private Button btnMenu;
+    private TextView tvRestaurantName;
+    private ImageView ivRestaurantLogo;
+    private Button btnStartOrder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnMenu = findViewById(R.id.btn_menu);
-        btnMenu.setOnClickListener(new View.OnClickListener() {
+        btnStartOrder = findViewById(R.id.btn_start_order);
+
+        btnStartOrder.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), ViewMenu.class);
                 startActivityForResult(intent,0);
