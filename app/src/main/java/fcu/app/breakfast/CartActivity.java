9c6d81436643331d8727504076be42e7bb7c236e
neoncatjacky <jackylanpoison97141@gmail.com>
2023-05-15 15:31:29 +0800

    adds a temp cart activity and adapter adds add to cart to menu fragment

---
+++ app/src/main/java/fcu/app/breakfast/CartActivity.java
@@ -0,0 +1,15 @@
+package fcu.app.breakfast;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CartActivity extends AppCompatActivity {
+
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.cart_activity);
+
+  };
+
+}
--- app/src/main/java/fcu/app/breakfast/fragments/MainDish.java
+++ app/src/main/java/fcu/app/breakfast/fragments/MainDish.java
@@ -1,24 +1,39 @@
 package fcu.app.breakfast.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import fcu.app.breakfast.CartActivity;
 import fcu.app.breakfast.R;
+import fcu.app.breakfast.ViewMenu;
 
 
-public class MainDish extends Fragment {
+public class MainDish extends Fragment implements View.OnClickListener {
 
-
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_dish, container, false);
+
+        view = inflater.inflate(R.layout.fragment_main_dish, container, false);
+        Button addBul = (Button) view.findViewById(R.id.button);
+        addBul.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(v.getContext(), CartActivity.class);
+        startActivityForResult(intent,0);
     }
 }
