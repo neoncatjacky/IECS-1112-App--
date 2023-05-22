f140d010ed751e9afb74678bf98d40e39f2fce49
Margot <margot.cheung@gmail.com>
2023-05-22 15:37:53 +0800

    checkout button - to be modified

--- app/src/main/java/fcu/app/breakfast/CartActivity.java
+++ app/src/main/java/fcu/app/breakfast/CartActivity.java
@@ -1,15 +1,32 @@
 package fcu.app.breakfast;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CartActivity extends AppCompatActivity {
 
+  private Button btnCheckout;
+
+
+  @Override
   protected void onCreate(Bundle savedInstanceState) {
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.cart_activity);
 
-  };
+    btnCheckout = findViewById(R.id.btn_checkout);
 
+    btnCheckout.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+          Intent intent = new Intent(v.getContext(), Checkout.class);
+          startActivityForResult(intent,0);
+        }
+    });
+    //btnCheckout.setOnClickListener(listener);
+  }
 }
